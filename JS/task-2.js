3de7f94a9c4d1cce7f882e5e8004a561817c8ea5
Yaroslav <110483805+Jaroslaav13@users.noreply.github.com>
2023-10-12 14:01:28 +0300

    Finish

---
+++ JS/task-2.js
@@ -0,0 +1,28 @@
+// _________________Task 2. Message formatting__________________
+
+function formatMessage(message, maxLength) {
+    if (message.length <= maxLength) {
+        return message
+    } else {
+        return message.slice(0, maxLength) + "...";
+    }
+       
+}
+console.log(formatMessage("Curabitur ligula sapien", 16));
+console.log(formatMessage("Curabitur ligula sapien", 23)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
+
+// ________Можна використати тернарний оператор_____
+
+// function formatMessage(message, maxLength) {
+//     return message.length <= maxLength ? message: message.slice(0, maxLength) + "...";
+// }
+// console.log(formatMessage("Curabitur ligula sapien", 16));
+// console.log(formatMessage("Curabitur ligula sapien", 23)); 
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); 
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); 
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); 
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
